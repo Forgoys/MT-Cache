77ab2d673773855cc269a95f16f31e90b1b8f5c2
Forgoys <578761491@qq.com>
2025-03-02 10:17:10 +0800

    fix bulk cache bugs

--- define_version/cache_bulk.h
+++ define_version/cache_bulk.h
@@ -1,56 +1,42 @@
 #include "hthread_device.h"
 
-// #define CACHEb_RD_K(__name, __Ea, __size, __type)                                                                      \
-//     char *_interface_buffer_##__name = (char *)scalar_malloc(__size + 64);                                             \
-//     unsigned long _interface_Ea_align_##__name = (unsigned long)__Ea & 0xFFFFFFFFFFFFFFE0;                             \
-//     unsigned long _interface_dma_offset_##__name = (unsigned long)__Ea - (unsigned long)_interface_Ea_align_##__name;  \
-//     unsigned long _interface_original_##__name;                                                                        \
-//     unsigned long _interface_sizes_##__name = (((__size + _interface_dma_offset_##__name) + 63) & ~63);                \
-//     scalar_load((void *)_interface_Ea_align_##__name, _interface_buffer_##__name, _interface_sizes_##__name);          \
-//     _interface_original_##__name = __Ea;                                                                               \
-//     __Ea = (__type *)((unsigned long)_interface_buffer_##__name + _interface_dma_offset_##__name);                     \
-//     unsigned long de_Ea_##__name = (unsigned long)__Ea;
-
-#define CACHEb_RD_K(__name, __Ea, __size, __type)                                                                      \
-    char *_interface_buffer_##__name = (char *)scalar_malloc(__size);                                             \
-    unsigned long _interface_Ea_align_##__name = (unsigned long)__Ea;                             \
-    unsigned long _interface_dma_offset_##__name = (unsigned long)__Ea - (unsigned long)_interface_Ea_align_##__name;  \
-    unsigned long _interface_original_##__name;                                                                        \
-    unsigned long _interface_sizes_##__name = __size;                \
-    scalar_load((void *)_interface_Ea_align_##__name, _interface_buffer_##__name, _interface_sizes_##__name);          \
-    _interface_original_##__name = __Ea;                                                                               \
-    __Ea = (__type *)((unsigned long)_interface_buffer_##__name + _interface_dma_offset_##__name);                     \
-    unsigned long de_Ea_##__name = (unsigned long)__Ea;
+#define CACHEb_RD_K(__name, __Ea, __size, __type)                                     \
+    unsigned long _interface_Ea_##__name = __Ea;\
+    unsigned long _interface_original_##__name = __name;                              \
+    unsigned long _interface_sizes_##__name = __size;                                 \
+    __type *_interface_buffer_##__name = (__type *)scalar_malloc(__size);             \
+    scalar_load((void *)__Ea, _interface_buffer_##__name, _interface_sizes_##__name); \
+    __name = (__type *)(unsigned long)_interface_buffer_##__name;
 
 // 批量写回内存
-#define CACHEb_WR_K(__name)                                                                                            \
-    do {                                                                                                               \
-        scalar_store(_interface_buffer_##__name, (void *)_interface_Ea_align_##__name, _interface_sizes_##__name);     \
-    } while (0)
+#define CACHEb_WR_K(__name)                                                                                      \
+    do {                                                                                                         \
+        scalar_store(_interface_buffer_##__name, (void *)_interface_Ea_##__name, _interface_sizes_##__name); \
+    } while (0);
 
-#define CACHEb_RD(__name, __addr, __value, __type)                                                                     \
-    do {                                                                                                               \
-        __value = *(__type *)(__addr);                                                                                 \
-    } while (0)
+#define CACHEb_RD(__name, __addr, __value, __type) \
+    do {                                           \
+        __value = *(__type *)(__addr);             \
+    } while (0);
 
-#define CACHEb_WT(__name, __addr, __value, __type)                                                                     \
-    do {                                                                                                               \
-        *(__type *)(__addr) = __value;                                                                                 \
-    } while (0)
+#define CACHEb_WT(__name, __addr, __value, __type) \
+    do {                                           \
+        *(__type *)(__addr) = __value;             \
+    } while (0);
 
-#define CACHEb_INIT(__name, __type, __Ea, __sets, __lines)                                                             \
-    unsigned long de_size_##__name = __lines;                                                                          \
+#define CACHEb_INIT(__name, __type, __Ea, __sets, __lines) \
+    unsigned long de_size_##__name = __lines;              \
     CACHEb_RD_K(__name, __Ea, de_size_##__name, __type);
 
-#define CACHEb_FLUSH(__name)                                                                                           \
-    do {                                                                                                               \
-        CACHEb_WR_K(__name);                                                                                           \
-        __name = _interface_original_##__name;                                                                         \
-        scalar_free(_interface_buffer_##__name);                                                                       \
-    } while (0)
+#define CACHEb_FLUSH(__name)                     \
+    do {                                         \
+        CACHEb_WR_K(__name);                     \
+        __name = _interface_original_##__name;   \
+        scalar_free(_interface_buffer_##__name); \
+    } while (0);
 
-#define CACHEb_INVALID(__name)                                                                                         \
-    do {                                                                                                               \
-        __name = _interface_original_##__name;                                                                         \
-        scalar_free(_interface_buffer_##__name);                                                                       \
-    } while (0)
+#define CACHEb_INVALID(__name)                   \
+    do {                                         \
+        __name = _interface_original_##__name;   \
+        scalar_free(_interface_buffer_##__name); \
+    } while (0);
